a890c694bc8717bcd887adde2f8829f2833120db
FranciscoAbad <unlafranciscoabad@gmail.com>
2022-05-12 18:32:58 -0300

    Se crea submenu3

--- scripts/submenu2.js
+++ scripts/submenu2.js
@@ -1,7 +1,21 @@
 let btnMenu = document.getElementById('nav__perfil');
 let mainNav = document.getElementById('nav__submenu');
+
+
 btnMenu.addEventListener('click', function(){
   mainNav.classList.toggle('mostrar');
+});
+
+let btnMenu3 = document.getElementById('submenu3__activador');
+let mainNav3 = document.getElementById('nav__submenu3');
+btnMenu3.addEventListener('click', function(){
+  mainNav3.classList.toggle('mostrar3');
+});
+
+let btnMenu32 = document.getElementById('submenu3__second-activate');
+let mainNav32 = document.getElementById('submenu3__second');
+btnMenu32.addEventListener('click', function(){
+  mainNav32.classList.toggle('mostrar3');
 });
 
 let btnMenu2 = document.getElementById('submenu2__active');
